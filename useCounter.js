40d7f96cf7afddc629b005129fb153aa3c85ccd5
Diegp <diegp2001@gmail.com>
2025-01-05 14:17:08 -0600

    first commit

---
+++ useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useCounter = (initValue = 5) =>{
+
+    const [counter, setCounter] = useState(initValue);
+
+    const increment = () =>{
+        setCounter(counter+1);
+    };
+    const decrement = () =>{
+        if(counter === 0) return;
+        setCounter( counter -1 );
+    };
+    const reset = () =>{
+        setCounter(initValue);
+    }
+
+    return{
+        counter,
+        increment, 
+        decrement,
+        reset,
+    };
+}
